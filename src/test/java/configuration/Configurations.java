5e2b455f547cc82df2e88f857761cb7e35a1d03e
soft-rma <rmaksymiv@softjourn.com>
2020-07-01 13:33:38 +0300

    Added browser configuration and simple test

--- src/test/java/configuration/Configurations.java
+++ src/test/java/configuration/Configurations.java
@@ -3,20 +3,16 @@ package configuration;
 import com.codeborne.selenide.Configuration;
 
 public class Configurations {
-	static Browsers DEFAULT_BROWSER = Browsers.CHROME;
 
 	public static void configureBrowser() {
-		switch(DEFAULT_BROWSER) {
-			case CHROME:{
-				Configuration.browser = "chrome";
+		switch(Configuration.browser) {
+			case "chrome":{
 				break;
 			}
-			case FIREFOX:{
-				Configuration.browser = "firefox";
+			case "firefox":{
 				break;
 			}
-			case IE:{
-				Configuration.browser = "ie";
+			case "ie":{
 				break;
 			}
 		}
