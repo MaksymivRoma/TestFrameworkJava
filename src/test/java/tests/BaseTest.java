6441c7a84c106fafcca62965d9b5f82b77bc1ade
soft-rma <rmaksymiv@softjourn.com>
2020-07-03 12:09:28 +0300

    Add allure report

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,14 +1,20 @@
 package tests;
 
-import configuration.Configurations;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 public abstract class BaseTest {
 
 	@BeforeAll
-	public static void baseTestBeforeClass() {
-		/*Configurations.configureBrowser();*/
+	public static void setUpSelenide() {
+		SelenideLogger.addListener("allure", new AllureSelenide().savePageSource(false));
 	}
 
+	@AfterAll
+	public static void tearDownSelenide() {
+		SelenideLogger.removeListener("allure");
+	}
 
 }
